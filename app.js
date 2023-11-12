540c27fedb44bbc322202222911232edcd391bd9
phuong090802 <20110547@student.hcmute.edu.vn>
2023-11-12 21:55:00 +0700

    Update

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
 
-app.get('/favicon.ico', (req, res) => res.status(204));
 app.use('/api/v1', products);
 app.use('/api/v1', auth);
 app.use('/api/v1', order);
