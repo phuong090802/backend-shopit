d41ca6c7af0c61c938e68fba387420cb8f4cf8c5
phuong090802 <20110547@student.hcmute.edu.vn>
2023-11-12 10:24:28 +0700

    Update

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,12 +1,12 @@
-import allowedOrigins from '../config/allowedOrigins.js'
+import allowedOrigins from '../config/allowedOrigins.js';
 
 export default function corsMiddleware(req, res, next) {
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
         res.header('Access-Control-Allow-Credentials', true);
         res.header('Access-Control-Allow-Origin', origin);
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type', 'Authorization');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Corrected line
     }
     next();
 }
