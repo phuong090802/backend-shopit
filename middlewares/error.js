154b5c710187ba1361912aae34a06dc0685dd579
phuong090802 <20110547@student.hcmute.edu.vn>
2023-11-12 20:22:57 +0700

    Update

--- middlewares/error.js
+++ middlewares/error.js
@@ -3,14 +3,6 @@ import ErrorHandler from '../utils/errorHandler.js';
 const errorMiddleware = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.message = err.message || 'Internal Server Error';
-    if (process.env.NODE_ENV === 'DEVELOPMENT') {
-        res.status(err.statusCode).json({
-            success: false,
-            error: err,
-            errorMessage: err.message,
-            stack: err.stack
-        });
-    }
 
     if (process.env.NODE_ENV === 'PRODUCTION') {
         let error = { ...err };
