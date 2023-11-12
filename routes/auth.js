611673e788cbb4dd55a2a1bdacc4c9359358153a
phuong090802 <20110547@student.hcmute.edu.vn>
2023-11-12 22:31:27 +0700

    Update

--- routes/auth.js
+++ routes/auth.js
@@ -23,7 +23,7 @@ router.route('/login').post(loginUser);
 router.route('/password/forgot').post(forgotPassword);
 router.route('/password/reset/:token').put(resetPassword);
 
-router.route('/logout').get(logout);
+router.route('/logout').post(logout);
 
 router.route('/me').get(isAuthenticatedUser, getUserProfile);
 
