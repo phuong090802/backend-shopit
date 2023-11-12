9aec7cd41440ab12cb7c2e4e7eb9ad69c8b033f3
phuong090802 <20110547@student.hcmute.edu.vn>
2023-11-12 18:20:47 +0700

    Update

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,5 +1,5 @@
 const sendToken = (user, statusCode, res) => {
-    // const token = user.getJwtToken();
+    const token = user.getJwtToken();
     const options = {
         // Date.now() trả về kiểu Number từ 1/1/1970
         expires: new Date(Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000),
@@ -7,10 +7,10 @@ const sendToken = (user, statusCode, res) => {
         path: '/api',
         sameSite: 'None',
         secure: true, // Thêm secure nếu sử dụng HTTPS,
-       
+
     }
 
-    res.status(statusCode).cookie('token', null, options).json({
+    res.status(statusCode).cookie('token', token, options).json({
         success: true,
         user
     });
