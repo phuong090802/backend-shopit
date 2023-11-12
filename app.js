dc584057b8c37b6b252eb8b77420ed4143e9329f
phuong090802 <20110547@student.hcmute.edu.vn>
2023-11-12 13:52:27 +0700

    Update

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import fileUpload from 'express-fileupload';
 import cors from 'cors';
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: '*',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
