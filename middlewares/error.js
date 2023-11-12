0db0b9ff7733151659e996cbd7a08ffa704e9456
phuong090802 <20110547@student.hcmute.edu.vn>
2023-11-12 20:36:46 +0700

    Update

--- middlewares/error.js
+++ middlewares/error.js
@@ -4,39 +4,38 @@ const errorMiddleware = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.message = err.message || 'Internal Server Error';
 
-    if (process.env.NODE_ENV === 'PRODUCTION') {
-        let error = { ...err };
-        error.message = err.message;
 
-        if (err.name === 'CastError') {
-            const message = `Resource not found. Invalid ${err.path}`;
-            error = new ErrorHandler(message, 400);
-        }
+    let error = { ...err };
+    error.message = err.message;
 
-        if (err.code === 11000) {
-            const message = `Duplicate ${Object.keys(err.keyValue)} entered`
-            error = new ErrorHandler(message, 400);
-        }
-
-        if (err.name === 'JsonWebTokenError') {
-            const message = 'JSON Web Token is invalid. Try Again!!!';
-            error = new ErrorHandler(message, 400);
-        }
-
-        if (err.name === 'TokenExpiredError') {
-            const message = 'JSON Web Token is expired. Try Again!!!';
-            error = new ErrorHandler(message, 400);
-        }
-
-        if (err.name === 'ValidationError') {
-            const message = Object.values(err.errors).map(value => value.message);
-            error = new ErrorHandler(message, 400);
-        }
-
-        res.status(error.statusCode).json({
-            success: false,
-            message: error.message || 'Internal Server Error'
-        });
+    if (err.name === 'CastError') {
+        const message = `Resource not found. Invalid ${err.path}`;
+        error = new ErrorHandler(message, 400);
     }
+
+    if (err.code === 11000) {
+        const message = `Duplicate ${Object.keys(err.keyValue)} entered`
+        error = new ErrorHandler(message, 400);
+    }
+
+    if (err.name === 'JsonWebTokenError') {
+        const message = 'JSON Web Token is invalid. Try Again!!!';
+        error = new ErrorHandler(message, 400);
+    }
+
+    if (err.name === 'TokenExpiredError') {
+        const message = 'JSON Web Token is expired. Try Again!!!';
+        error = new ErrorHandler(message, 400);
+    }
+
+    if (err.name === 'ValidationError') {
+        const message = Object.values(err.errors).map(value => value.message);
+        error = new ErrorHandler(message, 400);
+    }
+
+    res.status(error.statusCode).json({
+        success: false,
+        message: error.message || 'Internal Server Error'
+    });
 }
 export default errorMiddleware;
